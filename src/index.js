3c82b0cef427f3243b552704cf863abdae3da382
Felipe Ribeiro <felipe.fwribeiro@gmail.com>
2022-03-07 20:46:36 -0300

    Creating a middleware to validate if the account exists

--- src/index.js
+++ src/index.js
@@ -8,9 +8,30 @@ app.listen(3000)
 // Fazendo com que meu servidor possa receber json na request
 app.use(express.json())
 
+/**
+ * MIDDLEWARES: uma Função que fica entre o request e o response
+ */
+// Criando o Middleware
+function verifyIfExistsAccountCPF(request, response, next){
+    const {cpf} = request.headers
+    const customer = customers.find( customer => customer.cpf === cpf)
+    if (!customer) {
+        return response.status(400).json({error: "That customer does not exist"})
+    }
+    // agora todas as rotas que implementarem esse middleware terão acesso a request.customer
+    request.customer = customer
+    return next()
+}
+// Usando o Middleware
+// app.use(verifyIfExistsAccountCPF) -> se eu quiser que todas as rotas abaixo do app.use() usem esse middleware
+//app.get("/statement/", verifyIfExistsAccountCPF, (request,response) quero que so essa rota use esse middleware
+
+
 app.get("/", (request,response) =>{
     response.send("Server is Working and It was started")
 })
+
+
 
 /**
  * cpf -> string -> receber na criação da conta
@@ -37,11 +58,8 @@ app.post("/account", (request,response) =>{
 
 })
 
-app.get("/statement/:cpf", (request,response) => {
-    const {cpf} = request.params
-    const customer = customers.find( customer => customer.cpf === cpf)
-    if (!customer) {
-        return response.status(400).json({error: "That customer does not exist"})
-    }
+app.get("/statement/", verifyIfExistsAccountCPF, (request,response) => {
+    const {customer} = request
     return response.json(customer.statement)
 })
+
